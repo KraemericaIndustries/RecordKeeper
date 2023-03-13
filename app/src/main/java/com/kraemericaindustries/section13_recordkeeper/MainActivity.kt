a412c91249007c46222ef02a59b39d551df3b2c0
Bob Kraemer <bob.kraemer.git@gmail.com>
2023-03-12 22:39:05 -0400

    Initial commit

---
+++ app/src/main/java/com/kraemericaindustries/section13_recordkeeper/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.kraemericaindustries.section13_recordkeeper
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationBarView
+import com.kraemericaindustries.section13_recordkeeper.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        Log.d("BobKraemer", "INSIDE onCreate()")
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+//    override fun onBackPressed() {
+//        super.onBackPressed()
+//        Toast.makeText(this, "Back button pressed", Toast.LENGTH_LONG).show()
+//    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.reset_running -> {
+            Toast.makeText(this, "Clicked Reset Running", Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.reset_cycling -> {
+            Toast.makeText(this, "Clicked Reset Cycling", Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.reset_all -> {
+            Toast.makeText(this, "Clicked Reset All", Toast.LENGTH_LONG).show()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun onRunningClicked(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+        return true
+    }
+
+    private fun onCyclingClicked(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.nav_cycling -> onCyclingClicked()
+        R.id.nav_running -> onRunningClicked()
+        else -> false
+    }
+}
---
+++ app/src/main/java/com/kraemericaindustries/section13_recordkeeper/RunningFragment.kt
@@ -0,0 +1,41 @@
+package com.kraemericaindustries.section13_recordkeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kraemericaindustries.section13_recordkeeper.databinding.FragmentRunningBinding
+
+class RunningFragment : Fragment() {
+
+    private lateinit var binding: FragmentRunningBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRunningBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.container5km.setOnClickListener { launchRunningRecordScreen("5km") }
+        binding.container10km.setOnClickListener { launchRunningRecordScreen("10km") }
+        binding.container15km.setOnClickListener { launchRunningRecordScreen("15km") }
+        binding.container20km.setOnClickListener { launchRunningRecordScreen("20km") }
+    }
+
+    private fun launchRunningRecordScreen(distance: String) {
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        intent.putExtra("Distance", distance)
+        startActivity(intent)
+    }
+}
